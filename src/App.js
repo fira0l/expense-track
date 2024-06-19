bccdcf7be17218d7cb0ef13e28a4c530c4c19a3c
FIRAOL ANBESSA <firaolanbessa170@gmail.com>
2024-06-19 05:27:41 -0700

    An expense tracker web page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Book Rental",
+      amount: 25.6,
+      date: new Date(2021, 6, 21),
+    },
+    {
+      id: "e2",
+      title: "Car rental",
+      amount: 250.6,
+      date: new Date(2021, 6, 21),
+    },
+    {
+      id: "e3",
+      title: "Accessory Rental",
+      amount: 482.6,
+      date: new Date(2021, 6, 21),
+    },
+    {
+      id: "e4",
+      title: "Room Rental",
+      amount: 1200.6,
+      date: new Date(2021, 6, 21),
+    },
+    {
+      id: "e5",
+      title: "Accessory Rental",
+      amount: 150.6,
+      date: new Date(2019, 4, 20),
+    },
+  ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expenses);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import ExpenseItem from "./ExpenseItem";
+
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
